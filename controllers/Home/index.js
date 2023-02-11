f940f0712c66f8678aca5461bada7f526a0f998a
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2023-02-12 02:45:36 +0600

    Home page hero section CRUD is ready

--- controllers/Home/index.js
+++ controllers/Home/index.js
@@ -1,42 +1,219 @@
 const connection = require('../../db')
 
+const Hero = {
+    create: async (req, res) => {
+        const values = [
+            req.body.home_hero.id,
+            req.body.home_hero.title,
+            req.body.home_hero.paragraph,
+            req.body.home_hero.image,
+            req.body.home_hero.form_title,
+            req.body.home_hero.form_button,
+            req.body.home_hero.form_phone
+        ]
+        const values2 = req.body.hero_service_card.map(i => {
+            return [i.id, i.image, i.title]
+        })
+        console.log({
+            values,
+            values2
+        })
+        connection.query(`REPLACE INTO home_hero (id,title,paragraph,image,form_title,form_button,form_phone) VALUES (?, ?, ?, ?, ?, ?, ?)`, values, (err, row) => {
+            if (err) {
+                res.status(500).send({
+                    error: true,
+                    data: err,
+                    message: `Something went wrong!`
+                })
+            }
+            connection.query(`REPLACE INTO hero_service_card (id,image,title) VALUES ?;`, [values2], (err, row) => {
+                if (err) {
+                    res.status(500).send({
+                        error: true,
+                        data: err,
+                        message: `Something went wrong!`
+                    })
+                }
+                res.status(200).send({
+                    error: false,
+                    data: [],
+                    message: `Data created successfully!`
+                })
 
-// All Text
-const AllText = {
-    createData: async (req, res) => {
+            })
+        })
+    },
+    read: async (req, res) => {
+        connection.query(`SELECT * FROM home_hero`, (err, home_heros) => {
+            if (err) {
+                res.status(500).send({
+                    error: true,
+                    data: err,
+                    message: `Something was wrong!`
+                })
+            }
 
-        
-        const keys = Object.keys(req.body.all_home_plain_content).map((key) => {
-            return key
-        }).join(",")
+            connection.query(`SELECT * FROM hero_service_card`, (err, hero_service_cards) => {
+                if (err) {
+                    res.status(500).send({
+                        error: true,
+                        data: err,
+                        message: `Something was wrong!`
+                    })
+                }
+                console.log({ hero_service_cards })
+                res.status(200).send({
+                    error: false,
+                    data: {home_heros, hero_service_cards},
+                    message: `Successfully get data.`
+                })
+            })
+        })
+    },
+    update: async (req, res) => {
+        connection.query(`
+        UPDATE home_hero SET 
+        title="${req.body.home_hero.title}", 
+        paragraph="${req.body.home_hero.paragraph}",
+        image="${req.body.home_hero.image}",
+        form_title="${req.body.home_hero.form_title}",
+        form_button="${req.body.home_hero.form_button}",
+        form_phone="${req.body.home_hero.form_phone}" WHERE id=${req.body.home_hero.id}`, (err, row1) => {
+            if (err) {
+                res.status(500).send({
+                    error: true,
+                    data: err,
+                    message: `Something went wrong!`
+                })
+            }
 
-        const values = Object.keys(req.body.all_home_plain_content).map((key) => {
-            return `'${req.body.all_home_plain_content[key]}'`
-        }).join(",")
+            req.body.hero_service_card.map((item) => {
+                connection.query(`
+                UPDATE hero_service_card SET 
+                image="${item.image}", 
+                title="${item.title}"
+                WHERE id=${item.id}`, (err, row2) => {
+                    if (err) {
+                        res.status(500).send({
+                            error: true,
+                            data: err,
+                            message: `Something was wrong!`
+                        })
+                    }
+                });
+            })
 
-
-        connection.query(`INSERT INTO all_home_plain_content (${keys}) VALUES (${values})`, function (error, results, fields) {
-            if (error) throw res.status(200).send({
-                error: false,
-                data: error,
-                message: 'Something went wrong!'
-            });
             res.status(200).send({
                 error: false,
-                data: results,
-                message: 'data created successfully!'
+                data: [],
+                message: `Updated successfully.`
             })
-        });
+
+        })
     },
-    getData: async (req, res) => {
+    delete: async (req, res) => {
+        connection.query(`DELETE FROM hero_service_card WHERE id=${req.body.id}`, (err, row) => {
+            if (err) {
+                res.status(500).send({
+                    error: true,
+                    data: err,
+                    message: `Something was wrong!`
+                })
+            }
+            res.status(200).send({
+                error: false,
+                data: row,
+                message: `Deleted successfully!`
+            })
+        })
+    }
+}
+
+
+const MobileApp = {
+    create: async () => {
 
     },
-    updateData: async (req, res) => {
+    read: async () => {
+
+    },
+    update: async () => {
+
+    },
+    delete: async () => {
 
     }
 }
 
 
+const ServiceApp = {
+    create: async () => {
+
+    },
+    read: async () => {
+
+    },
+    update: async () => {
+
+    },
+    delete: async () => {
+
+    }
+}
+
+
+const Benifit = {
+    create: async () => {
+
+    },
+    read: async () => {
+
+    },
+    update: async () => {
+
+    },
+    delete: async () => {
+
+    }
+}
+
+
+const Location = {
+    create: async () => {
+
+    },
+    read: async () => {
+
+    },
+    update: async () => {
+
+    },
+    delete: async () => {
+
+    }
+}
+
+
+const CustomerSpeak = {
+    create: async () => {
+
+    },
+    read: async () => {
+
+    },
+    update: async () => {
+
+    },
+    delete: async () => {
+
+    }
+}
+
 module.exports = {
-    AllText,
+    Hero,
+    MobileApp,
+    ServiceApp,
+    Benifit,
+    Location,
+    CustomerSpeak,
 }
--- routes/Home/index.js
+++ routes/Home/index.js
@@ -1,9 +1,40 @@
 const router = require("express").Router();
 const Home = require('../../controllers/Home')
 
-// HeroAllContent
-router.post("/create", Home.AllText.createData);
-router.get("/", Home.AllText.getData);
-router.put("/update", Home.AllText.updateData);
+// Hero
+router.post("/hero/create", Home.Hero.create);
+router.get("/hero/read", Home.Hero.read);
+router.put("/hero/update", Home.Hero.update);
+router.delete("/hero_card/delete", Home.Hero.delete);
+
+// MobileApp
+router.post("/mobile_app/create", Home.MobileApp.create);
+router.get("/mobile_app/read", Home.MobileApp.read);
+router.put("/mobile_app/update", Home.MobileApp.update);
+router.delete("/mobile_app/delete", Home.MobileApp.delete);
+
+// ServiceApp
+router.post("/service_app/create", Home.ServiceApp.create);
+router.get("/service_app/read", Home.ServiceApp.read);
+router.put("/service_app/update", Home.ServiceApp.update);
+router.delete("/service_app/delete", Home.ServiceApp.delete);
+
+// Benifit
+router.post("/benifit/create", Home.Benifit.create);
+router.get("/benifit/read", Home.Benifit.read);
+router.put("/benifit/update", Home.Benifit.update);
+router.delete("/benifit/delete", Home.Benifit.delete);
+
+// Location
+router.post("/location/create", Home.Location.create);
+router.get("/location/read", Home.Location.read);
+router.put("/location/update", Home.Location.update);
+router.delete("/location/delete", Home.Location.delete);
+
+// CustomerSpeak
+router.post("/customer_speak/create", Home.CustomerSpeak.create);
+router.get("/customer_speak/read", Home.CustomerSpeak.read);
+router.put("/customer_speak/update", Home.CustomerSpeak.update);
+router.delete("/customer_speak/delete", Home.CustomerSpeak.delete);
 
 module.exports  = router;
